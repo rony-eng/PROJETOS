e31dae75c372eac8c215bac5dcda7b783a1f77ca
Rony-Menezes <83619784+Rny-hub@users.noreply.github.com>
2023-05-23 10:29:07 -0300

    JavaScript - Layout Missa
    Funcionamento dos inputs da página de missas e intenções, para adicionar as informações e limpar os inputs depois.

--- PJ SÃO VICENTE/CODE-TESTE/treinando_home_page.js
+++ PJ SÃO VICENTE/CODE-TESTE/treinando_home_page.js
@@ -109,6 +109,34 @@ function bnt_missa(){
         div_configurações.style.display = 'none';
     }
 }
+function adc_missa_intencoes()
+{
+    var input_missa = document.getElementById('inp1_div_missa').value;
+    var input_data = document.getElementById('inp2_div_missa').value;
+    var input_hora = document.getElementById('inp3_div_missa').value;
+    var input_intencoes = document.getElementById('inp4_div_missa').value;
+
+
+    if(input_missa && input_data && input_hora && input_intencoes != '')
+    {
+        alert('A missa é: ' + input_missa);
+        alert('A data da missa é: ' + input_data);
+        alert('O horário é: ' + input_hora);
+        alert('Intenções e falecidos da missa são:\n' + '\n' + input_intencoes);
+
+        var input_missa = document.getElementById('inp1_div_missa').value='';
+        var input_data = document.getElementById('inp2_div_missa').value='';
+        var input_hora = document.getElementById('inp3_div_missa').value='';
+        var input_intencoes = document.getElementById('inp4_div_missa').value='';
+
+        alert('Intenções adicionadas!')
+
+    }
+    else
+    {
+        alert('Campos faltam serem preenchidos!')
+    }
+}  
 
 // aqui fica voltado para a página de eventos
 function bnt_eventos(){
