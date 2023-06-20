b23d849639e13a18b4aada6973bfed91f1c835ec
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-06-20 18:39:23 -0300

    ATUALIZAÇÕES CSS
    Atualizações no CSS para página de home page do Projeto de São Vicente

--- PJ SÃO VICENTE/CODE-TESTE/treinando_home_page.js
+++ PJ SÃO VICENTE/CODE-TESTE/treinando_home_page.js
@@ -48,33 +48,12 @@ function bnt_sair_historia_sv(){
 
 // aqui fica voltado para a página de perfil
 function bnt_perfil(){
-    var container = document.querySelector('.div_perfil')
-    var divp = document.querySelector('.div_hp')
-    var div_historia_sv = document.querySelector('.historia_sv')
-    var div_missa = document.querySelector('.div_missa')
-    var div_eventos = document.querySelector('.div_eventos')
-    var div_calendário = document.querySelector('.div_calendario')
-    var div_arquivos = document.querySelector('.div_arquivos')
-    var div_configurações = document.querySelector('.div_configuracoes')
+    var container = document.querySelector('.div_perfil');
 
     if(container.style.display === 'none'){
         container.style.display = 'flex';
-        divp.style.display = 'none';
-        div_historia_sv.style.display = 'none';
-        div_missa.style.display = 'none';
-        div_eventos.style.display = 'none';
-        div_calendário.style.display = 'none';
-        div_arquivos.style.display = 'none';
-        div_configurações.style.display = 'none';
     }else{
         container.style.display = 'none';
-        divp.style.display = 'flex';
-        div_historia_sv.style.display = 'none';
-        div_missa.style.display = 'none';
-        div_eventos.style.display = 'none';
-        div_calendário.style.display = 'none';
-        div_arquivos.style.display = 'none';
-        div_configurações.style.display = 'none';
     }
 }
 
@@ -121,11 +100,6 @@ function adc_missa_intencoes()
 
     if(input_missa && input_data && input_hora && input_intencoes != '')
     {
-        alert('A missa é: ' + input_missa);
-        alert('A data da missa é: ' + input_data);
-        alert('O horário é: ' + input_hora);
-        alert('Intenções e falecidos da missa são:\n' + '\n' + input_intencoes);
-
         var input_missa = document.getElementById('inp1_div_missa').value='';
         var input_data = document.getElementById('inp2_div_missa').value='';
         var input_hora = document.getElementById('inp3_div_missa').value='';
@@ -249,13 +223,6 @@ function bnt_arquivos(){
 // aqui fica voltado para a página de configurações
 function bnt_configuracoes(){
     var container = document.querySelector('.div_configuracoes');
-    var divp = document.querySelector('.div_hp')
-    var div_historia_sv = document.querySelector('.historia_sv')
-    var div_perfil = document.querySelector('.div_perfil')
-    var div_eventos = document.querySelector('.div_eventos')
-    var div_calendário = document.querySelector('.div_calendario')
-    var div_arquivos = document.querySelector('.div_arquivos')
-    var div_missa = document.querySelector('.div_missa')
 
     if(container.style.display === 'none'){
         container.style.display = 'flex';
--- TESTES/TESTE.js
+++ TESTES/TESTE.js
@@ -14,3 +14,23 @@ function enviar(){
         textarea.value = inf_input
     }
 }
+
+if(input_missa && input_data && input_hora && input_intencoes != '')
+    {
+        alert('A missa é: ' + input_missa);
+        alert('A data da missa é: ' + input_data);
+        alert('O horário é: ' + input_hora);
+        alert('Intenções e falecidos da missa são:\n' + '\n' + input_intencoes);
+
+        var input_missa = document.getElementById('inp1_div_missa').value='';
+        var input_data = document.getElementById('inp2_div_missa').value='';
+        var input_hora = document.getElementById('inp3_div_missa').value='';
+        var input_intencoes = document.getElementById('inp4_div_missa').value='';
+
+        alert('Intenções adicionadas!')
+
+    }
+    else
+    {
+        alert('Campos faltam serem preenchidos!')
+    }
